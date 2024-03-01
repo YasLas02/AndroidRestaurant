d7100acd56645061ffb5f8353a18c511f37f968d
Yassine LASRI <yassinelasri02@gmail.com>
2024-03-01 12:27:20 +0100

    ajouter

---
+++ AndroidResto/app/src/main/java/fr/isen/lasri/androiderestaurant/HomeActivity.kt
@@ -0,0 +1,166 @@
+package fr.isen.lasri.androiderestaurant
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import fr.isen.lasri.androiderestaurant.ui.theme.AndroidERestaurantTheme
+
+enum class MenuType {
+    STARTER,
+     MAIN,
+    DESSERT;
+
+    @Composable
+    fun title(): String {
+        return when(this) {
+            STARTER -> stringResource(id = R.string.menu_entree)
+            MAIN -> stringResource(id = R.string.menu_plat)
+            DESSERT -> stringResource(id = R.string.menu_dessert)
+        }
+    }
+}
+
+interface MenuInterface {
+    fun displayMenu(dishType:MenuType )
+
+}
+class HomeActivity : ComponentActivity(), MenuInterface {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidERestaurantTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+
+                ) {
+                    SetupView(this)
+                }
+            }
+        }
+        Log.d("lifeCycle", "Home Activity - OnCreate")
+    }
+
+    override fun onPause() {
+        Log.d("lifeCycle", "Home Activity - OnPause")
+        super.onPause()
+    }
+    override fun displayMenu(dishType:MenuType ){
+        val intent = Intent(this, MenuActivity::class.java)
+        intent.putExtra(MenuActivity.CATEGROY_EXTRA_KEY, dishType)
+        startActivity(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("lifeCycle", "Home Activity - OnResume")
+    }
+
+    override fun onDestroy() {
+        Log.d("lifeCycle", "Home Activity - onDestroy")
+        super.onDestroy()
+    }
+}
+@Composable
+fun SetupView(menu: MenuInterface) {
+    val context = LocalContext.current
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo_resto),
+                contentDescription = null,
+                modifier = Modifier.size(80.dp)
+            )
+            Spacer(modifier = Modifier.width(25.dp))
+            Text(stringResource(R.string.app_name))
+            Spacer(modifier = Modifier.width(70.dp))
+            Row {
+                Icon(Icons.Filled.AccountBox, contentDescription = "Compte utilisateur",
+                    modifier = Modifier
+                        .clickable { /* TODO*/ }
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Icon(Icons.Filled.ShoppingCart, contentDescription = "panier",
+                    modifier = Modifier
+                        .clickable {
+                            val intent = Intent(context, PanierActivity::class.java)
+                            context.startActivity(intent)
+                        }
+                        .weight(1f)
+                )
+            }
+        }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextButton(onClick = { /* TODO */ }) {
+                Text("Tous")
+            }
+            CustomButton(type = MenuType.STARTER, menu)
+            CustomButton(type = MenuType.MAIN, menu)
+            CustomButton(type =MenuType.DESSERT, menu)
+        }
+
+    }
+
+}
+
+
+
+@Composable fun CustomButton(type: MenuType, menu: MenuInterface) {
+    TextButton(onClick = { menu.displayMenu(type) }) {
+        Text(type.title())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidERestaurantTheme {
+        SetupView(HomeActivity())
+    }
+}
+
+
+
